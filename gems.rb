c35efee9a27f6bd5bc56ed2cda3642ddbdff94e7
Franklin Yu <franklinyu@hotmail.com>
2019-05-19 23:40:55 -0400

    Fix test
    Add a formatter that works for most CI.

--- gems.rb
+++ gems.rb
@@ -7,7 +7,6 @@ gem 'haml', '~> 5.1'
 gem 'json-jwt', '1.10.1'
 gem 'mail', '~> 2.7'
 gem 'rake'
-gem 'rspec', '~> 3.8', group: :test
 gem 'sinatra', '~> 2.0'
 gem 'sinatra-activerecord', '~> 2.0'
 gem 'sqlite3', group: [:development, :test]
@@ -16,6 +15,11 @@ gem 'yard', group: :doc
 group :development do
   gem 'guard', '~> 2.15'
   gem 'guard-rack', '~> 2.2'
+end
+
+group :test do
+  gem 'rspec', '~> 3.8'
+  gem 'rspec_junit_formatter'
 end
 
 group :production do
